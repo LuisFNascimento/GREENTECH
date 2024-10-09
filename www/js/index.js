5ac850561b0073a902c01cd1717648266be8a980
Luís Fernando Nascimento <luisfernandonascimentopro@gmail.com>
2024-10-09 19:24:58 -0300

    Acrescentado o Back-End no projeto

---
+++ www/js/index.js
@@ -0,0 +1,46 @@
+fetch("js/backend.json")
+  .then((response) => response.json())
+  .then((data) => {
+    //SALVAR OS DADOS VINDOS DO BACK-END LOCALMENTE
+    //VAMOS UTILIZAR LOCALSTORAGE
+    localStorage.setItem("produtos", JSON.stringify(data));
+    console.log("Dados dos produtos salvos no localStorage");
+
+    setTimeout(() => {
+      //ESVAZIAR A ÁREA DE PRODUTOS
+      $("#produtos").empty();
+
+      data.forEach((produto) => {
+        var produtoHTML = `
+                   <!--ITEM CARD-->
+                        <div class="item-card">
+                            <a data-id="${
+                              produto.id
+                            }" href="/detalhes/" class="item">
+                                <div class="img-container">
+                                    <img src="${produto.imagem}">
+                                </div>
+                                <div class="nome-rating">
+                                    <span class="color-gray">${
+                                      produto.nome
+                                    }</span>
+                                    <span class="bold margin-right">
+                                        <i class="mdi mdi-star"></i>
+                                        ${produto.rating}
+                                    </span>
+                                </div>
+                                <div class="price">
+                                    ${produto.preco_promocional.toLocaleString(
+                                      "pr-Br",
+                                      { style: "currency", currency: "BRL" }
+                                    )}
+                                </div>
+                            </a>
+                        </div>
+            `;
+
+        $("#produtos").append(produtoHTML);
+      });
+    }, 1500);
+  })
+  .catch((error) => console.error("Erro ao fazer fetch dos dados: " + error));
