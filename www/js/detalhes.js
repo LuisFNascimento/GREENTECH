786602e89492e49b560c4ed0895c75e227592fa8
Luís Fernando Nascimento <luisfernandonascimentopro@gmail.com>
2024-11-11 18:50:48 -0300

    Automatizando os preços e detalhes dos produtos

--- www/js/detalhes.js
+++ www/js/detalhes.js
@@ -12,6 +12,12 @@ if (item) {
     //ALIMENTAR COM OS VALORES DO ITEM
     $("#imagem-detalhe").attr('src', item.imagem);
     $("#nome-detalhe").html(item.nome);
+    $("#rating-detalhe").html(item.rating);
+    $("#like-detalhe").html(item.likes);
+    $("#review-detalhe").html(item.reviews);
+    $("#descricao-detalhe").html(item.descricao);
+    $("#preco-detalhe").html(item.preco);
+    $("#preco_promocao-detalhe").html(item.preco_promocional);
 
 } else {
     console.log('Produto não encontrado');
