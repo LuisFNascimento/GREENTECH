e1c6d31e4f987e402479a071691f9c4affc6523f
Luís Fernando Nascimento <luisfernandonascimentopro@gmail.com>
2024-11-29 22:35:24 -0300

    criando a página carrinho.js

--- www/js/routes.js
+++ www/js/routes.js
@@ -33,7 +33,7 @@ var app = new Framework7({
 		},
 		pageInit: function (event, page) {
 		// fazer algo quando a página for inicializada
-    //app.views.main.router.navigate('/detalhes/');
+    app.views.main.router.navigate('/carrinho/');
     $.getScript('js/index.js');
 
     var swiper = new Swiper(".mySwiper", {
@@ -187,6 +187,7 @@ var app = new Framework7({
 		},
 		pageInit: function (event, page) {
 		// fazer algo quando a página for inicializada
+    $.getScript('js/carrinho.js');
 		},
 		pageBeforeRemove: function (event, page) {
 		// fazer algo antes da página ser removida do DOM
